446a35c21b61a8fc54f1333ac01c5d05888787eb
carajprethvi-compliance <carajprethvi@gmail.com>
2025-09-08 12:13:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Replace these URLs with your actual hosted file URLs
     const downloadUrls = {
-        windows: 'https://github.com/carajprethvi-compliance/NCEXPRESS_DOWNLOADS_V1.0.0/releases/download/v1.0.0/NCEXPRESS_v1.0.6_Setup.exe', // Direct download link to Windows executable
+        windows: 'https://github.com/carajprethvi-compliance/NCEXPRESS_DOWNLOADS_V1.0.0/releases/download/v1.0.0/NCEXPRESS_SETUP_v1.0.7.rar', // Direct download link to Windows executable
         mac: '#' // Replace with actual macOS download URL
     };
 
@@ -148,6 +148,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
 
 
 
